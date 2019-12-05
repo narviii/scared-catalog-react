330cd640ac70d455f899e097b3c0ab8ae0f59215
Vladimir Sofronov <narviii@gmail.com>
2019-12-04 21:26:04 -0800

    Updates

---
+++ precache-manifest.dda6535f6e4228dcd8133ab7ce9e78d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fac45f2e6f599cdf554c3042d11644d5",
+    "url": "/scared-catalog-react/index.html"
+  },
+  {
+    "revision": "14461928480b366e7886",
+    "url": "/scared-catalog-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4ae38275aa01951a6d0f",
+    "url": "/scared-catalog-react/static/js/2.69c5ea88.chunk.js"
+  },
+  {
+    "revision": "14461928480b366e7886",
+    "url": "/scared-catalog-react/static/js/main.056a8f37.chunk.js"
+  },
+  {
+    "revision": "371c05e3407c24b85056",
+    "url": "/scared-catalog-react/static/js/runtime-main.7c829c4b.js"
+  }
+]);
